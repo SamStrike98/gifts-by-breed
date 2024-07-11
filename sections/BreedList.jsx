4f7ece1dcd3984cf2453c61e3e1746ead9b457b0
Sam Strike <sam.strike1@gmail.com>
2024-07-11 16:51:33 +0100

    Initial setup

---
+++ sections/BreedList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BreedList = () => {
+    return (
+        <div>BreedList</div>
+    )
+}
+
+export default BreedList
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        main: "#FF7043",
+        secondary: "#A5D6A7",
+        accent: "#FFFFFF"
+      }
     },
   },
   plugins: [],
