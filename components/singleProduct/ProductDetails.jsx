60ca1ae010464042d20b14aae591d8e75f360e37
Sam Strike <sam.strike1@gmail.com>
2024-07-15 21:21:44 +0100

    Re-deploying

--- components/singleProduct/ProductDetails.jsx
+++ components/singleProduct/ProductDetails.jsx
@@ -6,7 +6,7 @@ const ProductDetails = ({ name, price }) => {
             <h2 className='text-3xl font-extrabold text-secondary'>{name}</h2>
             <div className='flex flex-row items-center gap-4 mt-3'>
                 <p className='text-xl font-bold text-white bg-primary p-3 rounded-md'>£{price.toFixed(2)}</p>
-                <p className='underline underline-offset-4 text-primary'>(£{(price * 0.3).toFixed(2)} goes to charity!)</p>
+                <p className='underline underline-offset-4 text-primary'>(£{(price * 0.3).toFixed(2)} goes to charity!!)</p>
             </div>
 
         </div>
